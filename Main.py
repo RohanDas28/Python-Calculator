a4dd19ac8400f865fc0802b8480ad49d80d3afee
Rasesh2005 <65321291+Rasesh2005@users.noreply.github.com>
2020-10-03 21:00:10 +0530

    Update main.py
    used lambda functions instead as the function had no operation except returning the result.. which should be generally done using lambda..
    
    Also added new Functionality of raisedToPower

--- Main.py
+++ Main.py
@@ -1,18 +1,18 @@
 #add two numbers 
-def add(num1, num2): 
-	return num1 + num2 
+add=lambda a,b:a+b
 
 #subtract two numbers 
-def subtract(num1, num2): 
-	return num1 - num2 
+subtract=lambda a,b:a-b
 
 #multiply two numbers 
-def multiply(num1, num2): 
-	return num1 * num2 
+multiply=lambda a,b:a*b
 
 #divide two numbers 
-def divide(num1, num2): 
-	return num1 / num2 
+divide=lambda a,b:a/b
+
+#one number raiseToPower another 
+raiseToPower=lambda a,b:a**b
+
 if __name__ == "__main__":
     while True:
         print("Please select operation -\n"
@@ -20,12 +20,13 @@ if __name__ == "__main__":
                 "2. Subtract\n" 
                 "3. Multiply\n"
                 "4. Multiply\n"
-                "5. Quit\n") 
+                "5. Raise to power of\n"
+                "6. Quit\n") 
 
 
         # Taking input from the user 
         select = input("Select operations form 1, 2, 3, 4, 5 :") 
-        if select == '5': 
+        if select == '6': 
             print("\n************ Thanks for using the calculator **************\n")
             exit() 
 
@@ -47,7 +48,9 @@ if __name__ == "__main__":
         elif select == '4': 
             print(number_1, "/", number_2, "=", 
                             divide(number_1, number_2))  
-        
+        elif select == '5': 
+            print(number_1, "^", number_2, "=", 
+                            raiseToPower(number_1, number_2))
         else: 
             print("Syntax Error!")  
         print("\n************ Thanks for using the calculator **************\n")

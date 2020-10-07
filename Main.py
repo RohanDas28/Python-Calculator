fc84aa35a627458f2a4a7f7f8f3e44bd1cd72f7a
Kamal Kumar Jena <72182167+Kamal-Kumar-Jena@users.noreply.github.com>
2020-10-07 11:39:12 +0530

    Update Main.py
    This an updated version of your code. I have added one another feature of finding square. Hope you like the feature.

--- Main.py
+++ Main.py
@@ -13,41 +13,59 @@ def multiply(num1, num2):
 #divide two numbers 
 def divide(num1, num2): 
 	return num1 / num2 
+#Finding aquare
+def square(num): 
+	return num*num
 if __name__ == "__main__":
     while True:
-        print("Please select operation -\n"
-                "1. Add\n"
-                "2. Subtract\n" 
-                "3. Multiply\n"
-                "4. Multiply\n"
-                "5. Quit\n") 
-
-
-        # Taking input from the user 
-        select = input("Select operations form 1, 2, 3, 4, 5 :") 
-        if select == '5': 
-            print("\n************ Thanks for using the calculator **************\n")
-            exit() 
-
-        number_1 = int(input("Enter first number: ")) 
-        number_2 = int(input("Enter second number: ")) 
-
-        if select == '1': 
-            print(number_1, "+", number_2, "=", 
-                            add(number_1, number_2)) 
-
-        elif select == '2': 
-            print(number_1, "-", number_2, "=", 
-                            subtract(number_1, number_2))  
-
-        elif select == '3': 
-            print(number_1, "*", number_2, "=", 
-                            multiply(number_1, number_2))  
-
-        elif select == '4': 
-            print(number_1, "/", number_2, "=", 
-                            divide(number_1, number_2))  
-        
-        else: 
-            print("Syntax Error!")  
-        print("\n************ Thanks for using the calculator **************\n")
+        try:
+	        print("Please select operation -\n"
+	                "1. Add\n"
+	                "2. Subtract\n" 
+	                "3. Multiply\n"
+	                "4. Divide\n"
+	                "5. Square\n"
+	                "6. Quit\n") 
+	
+	
+	        # Taking input from the user 
+	        select = input("Select operations from 1, 2, 3, 4, 5, 6 :") 
+	        if select== '5':
+	        	number = int(input("Enter the number: "))
+	        	sq=square(number)
+	        	print(f"The square of {number} is {sq}")
+	        	print("\n************ Thanks for using the calculator **************\n")
+	        	continue
+	        if select == '6': 
+	            print("\n************ Thanks for using the calculator **************\n")
+	            exit() 
+	
+	        number_1 = int(input("Enter first number: ")) 
+	        number_2 = int(input("Enter second number: ")) 
+	
+	        if select == '1': 
+	            print(number_1, "+", number_2, "=", 
+	                            add(number_1, number_2)) 
+	
+			      
+	        elif select == '2': 
+	            print(number_1, "-", number_2, "=", 
+	                            subtract(number_1, number_2))  
+	
+	        elif select == '3': 
+	            print(number_1, "*", number_2, "=", 
+	                            multiply(number_1, number_2))  
+	
+	        elif select == '4': 
+	            print(number_1, "/", number_2, "=", 
+	                            divide(number_1, number_2))  
+	         	
+	        
+	        else: 
+	            print("Syntax Error!")  
+	        print("\n************ Thanks for using the calculator **************\n")
+	        continue
+        except ValueError:
+	         print("Enter a Integer only.")
+	         continue
+	        

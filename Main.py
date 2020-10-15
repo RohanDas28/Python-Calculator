5a2a35c76e41044fe30c7181e60de1182c04b4ee
muteenk <62096896+muteenk@users.noreply.github.com>
2020-10-15 19:27:02 +0530

    created average function

--- Main.py
+++ Main.py
@@ -22,31 +22,40 @@ def divide():
     num2 = float(input("Enter number 2: "))
     print(f"The division of {num1} and {num2} results in {num1 / num2}")
 
+#exponent function
 def exponent():
     num = float(input("Enter number to be exponented: "))
     power = float(input("Enter power to be raised at: "))
     print(f"{num} raised to {power} is {num ** power}")
 
-def sqrt(): # sqrt -> square root
+#square root
+def sqrt(): 
     num = float(input("Enter number whose square root you want to know: "))
     print(f"Square root of {num} is {num ** 0.5}")
 
-def cbrt(): # cbrt -> cube root
+#cube root
+def cbrt(): 
     num = float(input("Enter number whose cube root you want to know: "))
     print(f"Cube root of {num} is {num ** (1/3)}")
+    
+def avg():
+    num1 = int(input("Enter a number"))
+    num2 = int(input("Enter another number"))
+    print(f"Average of {num1} and {num2} is {(num1+num2)/2}")
 
 if __name__ == "__main__":
     while True:
         # Taking input from the user 
-        select = int(input("Please select operation -\n"
-                "1. Add\n"
-                "2. Subtract\n" 
-                "3. Multiply\n"
-                "4. Divide\n"
-                "5. Exponentation\n"
-                "6. Square Root\n"
-                "7. Cube Root\n"
-                "8. Quit\n"))
+        select = int(input("Please select operation =>\n",
+                "1. Add\n",
+                "2. Subtract\n", 
+                "3. Multiply\n",
+                "4. Divide\n",
+                "5. Exponentation\n",
+                "6. Square Root\n",
+                "7. Cube Root\n",
+                "8. Average of two numbers\n",
+                "9. Quit\n"))
 
         if select == 1:
             add()
@@ -70,6 +79,9 @@ if __name__ == "__main__":
             cbrt()
 
         elif select == 8:
+            avg()
+            
+        elif select == 9:
             print("Thanks for visiting!")
             quit()
 

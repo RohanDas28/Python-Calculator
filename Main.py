7ab330db52e7c4233f8bcc534c0a0cd9e9a0cad3
Vyom Jain <vyom.j@protonmail.com>
2020-10-16 12:38:33 +0530

    Linted the code

--- Main.py
+++ Main.py
@@ -1,52 +1,60 @@
-#add two numbers 
-def add(): 
+# add two numbers
+def add():
     num1 = float(input("Enter number 1: "))
     num2 = float(input("Enter number 2: "))
     print(f"The sum of {num1} and {num2} is {num1 + num2}")
 
-#subtract two numbers 
-def subtract(): 
+
+# subtract two numbers
+def subtract():
     num1 = float(input("Enter number 1: "))
     num2 = float(input("Enter number 2: "))
     print(f"The difference of {num1} and {num2} is {num1 - num2}")
 
-#multiply two numbers 
-def multiply(): 
+
+# multiply two numbers
+def multiply():
     num1 = float(input("Enter number 1: "))
     num2 = float(input("Enter number 2: "))
     print(f"The product of {num1} and {num2} is {num1 * num2}")
 
-#divide two numbers 
-def divide(): 
+
+# divide two numbers
+def divide():
     num1 = float(input("Enter number 1: "))
     num2 = float(input("Enter number 2: "))
     print(f"The division of {num1} and {num2} results in {num1 / num2}")
+
 
 def exponent():
     num = float(input("Enter number to be exponented: "))
     power = float(input("Enter power to be raised at: "))
     print(f"{num} raised to {power} is {num ** power}")
 
-def sqrt(): # sqrt -> square root
+
+def sqrt():  # sqrt -> square root
     num = float(input("Enter number whose square root you want to know: "))
     print(f"Square root of {num} is {num ** 0.5}")
 
-def cbrt(): # cbrt -> cube root
+
+def cbrt():  # cbrt -> cube root
     num = float(input("Enter number whose cube root you want to know: "))
     print(f"Cube root of {num} is {num ** (1/3)}")
+    print('ssss')
+
 
 if __name__ == "__main__":
     while True:
-        # Taking input from the user 
+        # Taking input from the user
         select = int(input("Please select operation -\n"
-                "1. Add\n"
-                "2. Subtract\n" 
-                "3. Multiply\n"
-                "4. Divide\n"
-                "5. Exponentation\n"
-                "6. Square Root\n"
-                "7. Cube Root\n"
-                "8. Quit\n"))
+            "1. Add\n"
+            "2. Subtract\n"
+            "3. Multiply\n"
+            "4. Divide\n"
+            "5. Exponentation\n"
+            "6. Square Root\n"
+            "7. Cube Root\n"
+            "8. Quit\n"))
 
         if select == 1:
             add()
@@ -73,7 +81,7 @@ if __name__ == "__main__":
             print("Thanks for visiting!")
             quit()
 
-        else: 
-            print("Invalid input!") 
+        else:
+            print("Invalid input!")
 
         print("\n************ Thanks for using the calculator **************\n")
